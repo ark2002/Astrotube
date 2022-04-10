39e1b20e01299ade537c920cce702f951d37eadf
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-04-10 20:45:58 +0530

    Feat : Connected Everything

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import { Footer, Navbar } from "./frontend/components";
+import { Router } from "./frontend/router/router";
 
 function App() {
   return (
     <div className="App">
- 
+      <Navbar/>
+      <Router/>
+      <Footer/>
     </div>
   );
 }
