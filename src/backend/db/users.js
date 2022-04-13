bd520fb30e16aa3f84f42e4ba659201e1e90fbd8
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-04-13 17:00:25 +0530

    feat : made user in the db

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -3,16 +3,15 @@ import { formatDate } from "../utils/authUtils";
 /**
  * User Database can be added here.
  * You can add default users of your wish with different attributes
- * Every user will have likes (Likes are set to 0 by default), History Array, Playlists Array (added Watch Later Playlist in it by default) by default
  * */
 
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    email: "adarshbalika@gmail.com",
-    password: "adarshBalika123",
+    firstName: "Guest",
+    lastName: "Guest",
+    email: "guest@gmail.com",
+    password: "guest123",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
