64ba37356f590d2b865405d325ae5a925897be25
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-04-23 17:30:20 +0530

    feat : made dummy listing page screen

--- src/frontend/screens/ListingScreen/ListingScreen.js
+++ src/frontend/screens/ListingScreen/ListingScreen.js
@@ -1,8 +1,30 @@
+import { ExplorePageCard } from "../../components";
+import "./ListingScreen.css";
+
 const ListingScreen = () => {
 
     return (
         <>
-            ListingScreen
+            <div className="explore__page-top flex--column">
+                <h1 className="heading3 primary__font page__heading">Explore Page</h1>
+                <div className="category__choices flex--row secondary__font">
+                    <span className="choice1">Fun Astro</span>
+                    <span className="choice2">Astro Facts</span>
+                    <span className="choice3">Horoscope 2022</span>
+                </div>
+            </div>
+            <div className="explore__list">
+                <ExplorePageCard />
+                <ExplorePageCard />
+                <ExplorePageCard />
+                <ExplorePageCard />
+                <ExplorePageCard />
+                <ExplorePageCard />
+                <ExplorePageCard />
+                <ExplorePageCard />
+                <ExplorePageCard />
+                <ExplorePageCard />
+            </div>
         </>
     );
 }
