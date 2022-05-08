29749327cac80d7c3889b1c42328fb57e0ed4263
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-05-08 12:33:54 +0530

    feat : made changes to Listing Screen

--- src/frontend/screens/ListingScreen/ListingScreen.js
+++ src/frontend/screens/ListingScreen/ListingScreen.js
@@ -1,29 +1,30 @@
 import { ExplorePageCard } from "../../components";
+import { UseFilter } from "../../context";
 import "./ListingScreen.css";
 
 const ListingScreen = () => {
 
+    const { filteredVideos, filter, setFilter } = UseFilter();
+
+    const FilterHandeler = (e) => {
+        if (filter === e.currentTarget.textContent) {
+            setFilter("Any")
+        } else {
+            setFilter(e.currentTarget.textContent);
+        }
+    }
     return (
         <>
             <div className="explore__page-top flex--column">
                 <h1 className="heading3 primary__font page__heading">Explore Page</h1>
                 <div className="category__choices flex--row secondary__font">
-                    <span className="choice1">Fun Astro</span>
-                    <span className="choice2">Astro Facts</span>
-                    <span className="choice3">Horoscope 2022</span>
+                    <span className={filter === "Fun Astro" ?"choice1__active":"choice1"} title="Fun Astro" onClick={FilterHandeler}>Fun Astro</span>
+                    <span className={filter === "Astro Facts" ?"choice2__active":"choice2"} title="Astro Facts" onClick={FilterHandeler}>Astro Facts</span>
+                    <span className={filter === "Horoscope 2022" ?"choice3__active":"choice3"} title="Horoscope 2022" onClick={FilterHandeler}>Horoscope 2022</span>
                 </div>
             </div>
             <div className="explore__list">
-                <ExplorePageCard />
-                <ExplorePageCard />
-                <ExplorePageCard />
-                <ExplorePageCard />
-                <ExplorePageCard />
-                <ExplorePageCard />
-                <ExplorePageCard />
-                <ExplorePageCard />
-                <ExplorePageCard />
-                <ExplorePageCard />
+                {filteredVideos.map((video) => <ExplorePageCard video={video} key={video._id} />)}
             </div>
         </>
     );
