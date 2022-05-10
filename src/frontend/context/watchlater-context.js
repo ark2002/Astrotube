9fe847bba8b727c909bf3810dd8a02a51d8c6078
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-05-10 18:38:41 +0530

    feat : added watchlater context

---
+++ src/frontend/context/watchlater-context.js
@@ -0,0 +1,34 @@
+import { useState, useEffect, useContext, createContext } from "react";
+import { useAuth } from "./auth-context";
+import { getWatchLater } from "../services";
+
+const WatchLaterContext = createContext();
+
+const WatchLaterProvider = ({ children }) => {
+
+    const [watchLater, setWatchLater] = useState([]);
+    const { auth } = useAuth();
+
+    useEffect(() => {
+        if (auth.isAuth) {
+            (async () => {
+                const response = await getWatchLater(auth.token);
+                if (response !== undefined) {
+                    setWatchLater(response);
+                } else {
+                    setWatchLater([]);
+                }
+            })();
+        }
+    }, [auth]);
+
+    return (
+        <WatchLaterContext.Provider value={{ watchLater, setWatchLater }} >
+            {children}
+        </WatchLaterContext.Provider>
+    );
+}
+
+const useWatchLater = () => useContext(WatchLaterContext);
+
+export { WatchLaterProvider, useWatchLater }
