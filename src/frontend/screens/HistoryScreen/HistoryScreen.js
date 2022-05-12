6cb2300e908287954b3f03f9d2bbceb0d9a24284
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-05-12 12:41:54 +0530

    feat : created history screen

--- src/frontend/screens/HistoryScreen/HistoryScreen.js
+++ src/frontend/screens/HistoryScreen/HistoryScreen.js
@@ -1,9 +1,36 @@
+import { HistoryPageCard } from "../../components";
+import { useAuth, useHistory } from "../../context";
+import { deleteAllFromHistory } from "../../services";
+import "./HistoryScreen.css"
+
 const HistoryScreen = () => {
 
+    const { history } = useHistory();
+    const { auth } = useAuth();
+    const { setHistory } = useHistory();
+
+    const deleteAllFromHistoryHandler = async () => {
+        const response = await deleteAllFromHistory(auth.token);
+        if (response !== undefined) {
+            setHistory(response);
+        } else {
+            setHistory([]);
+        }
+    }
+
     return (
-        <>
-            HistoryScreen
-        </>
+        <div className="flex--column history__screen">
+            {history.length > 0 ? <div className="history__page flex--column">
+                <h1 className="heading3 primary__font history__heading">Your Watch History</h1>
+                <div className="history-videos__list flex--column">
+                    {history.map((video) => <HistoryPageCard video={video} key={video._id} />)}
+                </div>
+                <button className="btn btn-color--primary btn-font--secondary btn__deleteAll" onClick={() => deleteAllFromHistoryHandler()}>Delete All</button>
+            </div> :
+                <div className="history__page flex--column">
+                    <h1 className="heading3 primary__font history__heading">Your Watch History will be shown here</h1>
+                </div>}
+        </div>
     );
 }
 
