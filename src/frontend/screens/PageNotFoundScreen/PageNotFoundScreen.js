3957a114e7d770631625798555b1f312ee408ca2
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-05-14 15:52:33 +0530

    feat : 404page added

--- src/frontend/screens/PageNotFoundScreen/PageNotFoundScreen.js
+++ src/frontend/screens/PageNotFoundScreen/PageNotFoundScreen.js
@@ -1,9 +1,16 @@
+import "./PageNotFoundScreen.css"
+
 const PageNotFoundScreen = () => {
 
     return (
-        <>
-            PageNotFoundScreen
-        </>
+        <div className="error-page__container flex--column">
+            <div className="page__title flex--row" >
+                <h1 className="page__main primary__font">4</h1>
+                <img src="/assets/zodiaclogo.png" alt="logo" className="page__hero" />
+                <h1 className="page__main primary__font">4</h1>
+            </div>
+            <h1 className="secondary__font heading1">Page Not Found</h1>
+        </div>
     );
 }
 
