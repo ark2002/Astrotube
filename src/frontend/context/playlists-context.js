9e08b29a12cf41a54f08a7212514115374d388e1
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-05-14 13:18:58 +0530

    feat : created playlist context

---
+++ src/frontend/context/playlists-context.js
@@ -0,0 +1,39 @@
+import { useReducer, useEffect, useContext, createContext } from "react";
+import { playlistsReducer } from "../reducer/playlistsReducer";
+import { getAllPlaylists } from "../services";
+import { useAuth } from "./auth-context";
+
+
+const PlaylistsContext = createContext();
+
+const PlaylistsProvider = ({ children }) => {
+
+    const { auth } = useAuth();
+
+    const [playlists, dispatchPlaylists] = useReducer(playlistsReducer, {
+        isPlaylist: false,
+        currentVideo: {},
+        playlistArray: []
+    })
+
+    useEffect(() => {
+        if (auth.isAuth) {
+            (async () => {
+                const response = await getAllPlaylists(auth.token);
+                if (response !== undefined) {
+                    dispatchPlaylists({ type: "setPlaylists", payload: response })
+                }
+            })()
+        }
+    }, [auth])
+
+    return (
+        <PlaylistsContext.Provider value={{ playlists, dispatchPlaylists }}>
+            {children}
+        </PlaylistsContext.Provider>
+    );
+}
+
+const usePlaylists = () => useContext(PlaylistsContext);
+
+export { PlaylistsProvider, usePlaylists };
