0bc7aa09907dd8938a676d2efaf49e97a17b97c2
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-05-17 20:24:53 +0530

    fix : explore button linked

--- src/frontend/screens/LandingScreen/LandingScreen.js
+++ src/frontend/screens/LandingScreen/LandingScreen.js
@@ -1,6 +1,9 @@
+import { useNavigate } from "react-router-dom";
 import "./LandingScreen.css";
 
 const LandingScreen = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="landing-page__container flex--column">
       <img
@@ -12,7 +15,12 @@ const LandingScreen = () => {
       <p className="landing-page__description heading4 secondary__font">
         A one place destination for all your Astro videos.
       </p>
-      <button className="primary__font landing-page__button">Explore</button>
+      <button
+        className="primary__font landing-page__button"
+        onClick={() => navigate("/explore")}
+      >
+        Explore
+      </button>
     </div>
   );
 };
